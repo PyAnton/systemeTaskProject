3abdfb4ef02bd7e21649180d6ab9539338c3a553
Antonmak112 <pyanton112@gmail.com>
2024-05-30 17:25:51 +0500

    Initial commit

---
+++ src/main/java/py/antonmak/systemetaskproject/SystemeTaskProjectApplication.java
@@ -0,0 +1,13 @@
+package py.antonmak.systemetaskproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeTaskProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemeTaskProjectApplication.class, args);
+    }
+
+}
